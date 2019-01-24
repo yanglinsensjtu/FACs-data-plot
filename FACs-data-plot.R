3e91ac50ef7c56a40d0a37485be2ba52ce5f65c0
yanglinsensjtu <yanglinsensjtu@126.com>
2019-01-24 11:37:26 +0800

    add comments

--- FACs-data-plot.R
+++ FACs-data-plot.R
@@ -3,6 +3,13 @@ gc()
 library(tidyverse)
 # Functions ---------------------------------------------------------------
 
+# Two arguments : files_name_list and treatment_names.
+# files_name_list contains the FACs data which are exported as CSV files, each CSV files 
+# represented as a sample acquired by FACs Guava, the two color acquired value are 
+# extracted from the CSV file.
+# Treatment_names are used as the samples names which were used in the data manipulation
+# and plot procedure.
+
 c_data <- function(files = file_name_list, treatment_names = tnames){
   nm <- vector('list',length(files))
   migb <- vector('list',length(files))
@@ -17,11 +24,23 @@ c_data <- function(files = file_name_list, treatment_names = tnames){
   }
   migb
 }
+# FACS_plot has five arguments data, plot_treat_names, plot_label_names, 
+# plot_title, xmin, xmax.
+# c_data function produced the data, which contains the relative color 
+# value in each column, column names are the sample names(treatment_names).
+# Plot_treat_names refer to the samples which are used to plot in the 
+# final figure are subset of treatment_names. Plot_label_names are 
+#the figure caption labels according to plot_treat_names. Plot_title 
+# is the figure title. Xmin or xmax is the minimum or maximum value of 
+# the x-axis.
+
 
 FACS_plot <- function(data = migbc, 
                       plot_treatment_names = ptn, 
                       plot_label_names = pln,
-                      plot_title = title, xmin = xmin, xmax = xmax){
+                      plot_title = title, 
+                      xmin = xmin, 
+                      xmax = xmax){
   total <- select(data, plot_treatment_names) %>% 
     gather(plot_treatment_names,
            key = "treatment",value = "cases") %>% 
