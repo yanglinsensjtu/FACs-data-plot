4e278a382a7193f8ef2361b5e5f062ff344578cc
yanglinsensjtu <yanglinsensjtu@126.com>
2019-02-21 17:10:52 +0800

    save the sample labels to the disk

--- execute.R
+++ execute.R
@@ -3,14 +3,15 @@ source('FACs-data-plot/functions.R')
 # Arguments ---------------------------------------------------------------
 
 file_name_list <- dir(pattern = "\\.CSV")
-
-file_name_list
-
 treat_names <- c()
+label_names <-  c()
+
+# save sample labels to disk ----------------------------------------------
 
 
-
-label_names <- c()
+fntn <-  C_filename_treatname(file_name_list,treat_names)
+unlistfntn <- unlist(fntn)
+write.csv(unlistfntn,file = 'lable.txt')
 
 # plot --------------------------------------------------------------------
 
@@ -19,27 +20,8 @@ data <- c_data(files = file_name_list, treatment_names = treat_names)
 FACS_plot(data = data,
           plot_treatment_names = treat_names,
           plot_label_names = label_names,
-          plot_title = '',
+          plot_title = 'total',
           xmin = 0.35,
           xmax = 0.9
 )
 
-FACS_plot(data = data,
-          plot_treatment_names = c(treat_names[1:2],treat_names[3:6]),
-          plot_label_names = c(label_names[1:2],label_names[3:6]),
-          plot_title = '',
-          xmin = 0.35,
-          xmax = 0.9
-)
-
-FACS_plot(data = data,
-          plot_treatment_names = c(treat_names[1:2],treat_names[7:10]),
-          plot_label_names = c(label_names[1:2],label_names[7:10]),
-          plot_title = '',
-          xmin = 0.35,
-          xmax = 0.9
-)
-
-fntn <-  C_filename_treatname(file_name_list,treat_names)
-
-unlist(fntn)
